c8405f447b4e3a1af6b9ec743508969586d2d686
Joseph Lozano <joseph@hey.com>
2020-09-07 01:31:17 -0400

    form

--- lib/temps_wtf/weather.ex
+++ lib/temps_wtf/weather.ex
@@ -1,9 +1,17 @@
 defmodule TempsWTF.Weather do
   alias TempsWTF.Weather.Station
   alias TempsWTF.Repo
-  import Ecto.Query, only: [from: 1]
+  import Ecto.Query, only: [from: 1, from: 2]
 
   def count_stations do
     Repo.aggregate(from(s in Station), :count)
+  end
+
+  def stations_by_country_and_region(country, region) do
+    Repo.all(
+      from s in Station,
+        where: [country: ^country, region: ^region],
+        order_by: :en_name
+    )
   end
 end
--- lib/temps_wtf_web/live/page_live.ex
+++ lib/temps_wtf_web/live/page_live.ex
@@ -1,39 +1,36 @@
 defmodule TempsWTFWeb.PageLive do
   use TempsWTFWeb, :live_view
+  alias TempsWTF.Weather
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    {:ok, assign(socket, stations: [], state: nil)}
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  def handle_event("get_stations", %{"location" => params}, socket) do
+    %{"country" => country, "state" => state} = params
+
+    stations =
+      if Enum.any?([country, state], &(&1 == "")) do
+        []
+      else
+        Weather.stations_by_country_and_region(country, state)
+      end
+
+    {:noreply, assign(socket, stations: stations, state: state)}
   end
 
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
+  def handle_event("lookup_station", %{"station" => station_id}, socket) do
+    {:noreply, socket}
   end
 
-  defp search(query) do
-    if not TempsWTFWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
+  defp states do
+    ["CA", "NY"]
+  end
 
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+  defp options_for(stations) do
+    stations
+    |> Enum.map(&{&1.en_name, &1.id})
   end
 end
